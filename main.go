6a012642dd21791a3b193979f73865d7f16d188a
Hiromasa428 <h428.kobasuke@gmail.com>
2018-07-03 18:51:19 +0900

    hello

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
-package main 
-import "fmt"
+package main
 
-func main(){
- fmt.Printlng("hey")
+import (
+    "os"
+    "fmt"
+    "github.com/ChimeraCoder/anaconda"
+)
+
+func main() {
+	fmt.Println(os.Getenv("CONSUMER_KEY"))
+	fmt.Println(os.Getenv("CONSUMER_SECRET"))
+	fmt.Println(os.Getenv("ACCESS_TOKEN"))
+	fmt.Println(os.Getenv("ACCESS_TOKEN_SECRET "))
+    anaconda.SetConsumerKey(os.Getenv("CONSUMER_KEY"))
+    anaconda.SetConsumerSecret(os.Getenv("CONSUMER_SECRET"))
+    api := anaconda.NewTwitterApi(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_TOKEN_SECRET"))
+    _,err := api.PostTweet("Goでボット書きました。",nil)
+    if err != nil{
+    	fmt.Println(err)
+    }
 }
